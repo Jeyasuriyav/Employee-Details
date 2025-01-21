02142a076218ddd8ca70542c84989ef64df87df2
Jeyasuriyav <tharapetro15@gmail.com>
2025-01-21 20:12:24 +0530

    qa

--- src/org/emp/Emp.java
+++ src/org/emp/Emp.java
@@ -15,6 +15,9 @@ public static void main(String[] args) {
 	Emp a=new Emp();
 	a.empID();
 a.empId("Raju", 200000);
+System.out.println("gitbashdone");
+System.out.println("done by QA");
+System.out.println("good");
 }
 
 }
